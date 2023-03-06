e8e3026c20b4af4393a84da235e8929e9c4177b0
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-03-06 16:58:09 +0300

    fst

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <div class="q-pa-md q-gutter-sm">
+    <div class="text-h4">
+      Коты
+      <q-badge align="top">cli v1.0.0</q-badge>
+    </div>
+  </div>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+
+export default defineComponent({
+  name: 'MainLayout',
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page >
+    <div class="q-pa-md row">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+      class="col-4 offset-4 carousel"
+    >
+      <q-carousel-slide :name="1" img-src="~assets/img/7.png" />
+      <q-carousel-slide :name="2" img-src="~assets/img/8.png" />
+      <q-carousel-slide :name="3" img-src="~assets/img/9.png" />
+    </q-carousel>
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup () {
+    return {
+      slide: ref(1)
+    }
+  }
+})
+</script>
+
+<style>
+  .carousel{
+    min-height: 300px;
+  }
+</style>
