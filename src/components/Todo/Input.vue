4399efef9444c7ef11a3f274f0f89d9b4bc66a62
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-03-10 00:22:17 +0300

    added task status functions

--- src/components/Todo/Input.vue
+++ src/components/Todo/Input.vue
@@ -2,41 +2,76 @@
   <div class="q-pa-lg">
         <q-input @keyup.enter="addTask" dark filled bottom-slots v-model="todoText" label="Add task">
         <template v-slot:append>
-          <q-btn @click="addTask" round dense flat icon="add" />
+          <q-btn v-for="item in statuses"
+          :key="item.id"
+          :icon="item.status"
+          round dense flat
+          @click="pickStatus(item.status,$event)"
+          ref="btn"
+          class="status"
+          />
+          <q-btn @click="addTask" icon="add" />
         </template>
       </q-input>
       </div>
 </template>
 
 <script>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import { useQuasar } from 'quasar';
-export default{
+export default {
   emits: ['addTask'],
-  setup (props, { emit }) {
+  setup(props, { emit }) {
     const $q = useQuasar();
+    const statuses = [
+      {
+        id: 1,
+        status: 'rocket'
+      },
+      {
+        id: 2,
+        status: 'history'
+      },
+      {
+        id: 3,
+        status: 'work'
+      },
+      {
+        id: 4,
+        status: 'favorite'
+      },
+    ];
+    // const btn=ref();
+    const status =ref('rocket');
     const todoText = ref('');
-    const addTask= ()=>{
-      if(todoText.value.trim()){
-      emit('addTask',todoText.value);
+    const addTask = () => {
+      if (todoText.value.trim()) {
+        emit('addTask', todoText.value,status.value);
       }
-      else{
-      $q.notify({
+      else {
+        $q.notify({
           message: 'Write something',
           icon: 'report_problem'
         })
       }
-      todoText.value='';
+      todoText.value = '';
+    };
+    const pickStatus=(text,e)=>{
+      document.querySelectorAll('.status i').forEach(e=>e.classList.remove('text-primary'));
+      status.value=text;
+      e.target.classList.add('text-primary');
     }
 
     return {
       todoText,
-      addTask
+      addTask,
+      status,
+      statuses,
+      pickStatus,
+      // btn
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
