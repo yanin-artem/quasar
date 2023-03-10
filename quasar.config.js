a056d6d1081aa31da1e6b66cdb399cfa54c870c1
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-03-10 15:51:38 +0300

    added an electron to create a desktop application

--- quasar.config.js
+++ quasar.config.js
@@ -195,6 +195,7 @@ module.exports = configure(function (ctx) {
       bundler: 'packager', // 'packager' or 'builder'
 
       packager: {
+        platform:'win32'
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
 
         // OS X / Mac App Store
